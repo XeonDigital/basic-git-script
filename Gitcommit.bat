27a8a1760f898fe27c944e9e1201e1bb7c89a7d8
XeonDigital <xinrongisaweosm2@gmail.com>
2020-03-04 18:29:43 -0500

    edited a few quotations

--- Gitcommit.bat
+++ Gitcommit.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 echo Press enter to continue without opening
-set /p "folder=Which folder are you trying to open(Enter folder path or folder name [folder name must be in the current folder])?"
+set /p folder="Which folder are you trying to open(Enter folder path or folder name [folder name must be in the current folder])?"
 cd %folder%
 cls
-set /p "input=Do you want to pull now (type y or enter for no)?"
+set /p input="Do you want to pull now (type y or enter for no)?"
 if %input%== y (goto pull)
 :start
 cls
@@ -42,7 +42,7 @@ pause
 goto end
 
 :switchRepo
-set /p "folder=Which folder are you trying to open(Enter folder path or folder name [folder name must be in the current folder])?"
+set /p folder="Which folder are you trying to open(Enter folder path or folder name [folder name must be in the current folder])?"
 cd %folder%
 goto start
 
@@ -62,23 +62,23 @@ goto start
 :merge
 echo The highlighted green text is where you are right now
 git branch
-set /p "merge=Which branch do you want to merge from?"
+set /p merge"=Which branch do you want to merge from?"
 git merge %merge%
 pause
 goto start
 
 :switch
 git branch
-set /p "set=Which branch do you want to switch to?"
+set /p set="Which branch do you want to switch to?"
 git checkout %set%
 pause
 goto start
 
 
 :createB
-set /p "branch=What would your branch name be?"
+set /p branch="What would your branch name be?"
 git checkout -b %branch%
-set /p "pull=Which branch do you want to pull from?"
+set /p pull="Which branch do you want to pull from?"
 git pull origin %pull%
 git commit -m "Created a new branch from %pull%"
 git push origin %branch%
@@ -87,7 +87,7 @@ goto start
 
 :commit
 cls
-set /p "reason=Enter your reason for this commit:"
+set /p reason="Enter your reason for this commit:"
 git add .
 git commit -m "%reason%"
 pause
@@ -96,7 +96,7 @@ goto push
 
 :push
 git branch
-set /p "branch=Which branch do you want to push to?"
+set /p branch="Which branch do you want to push to?"
 git push origin %branch%
 pause
 goto start
@@ -104,16 +104,16 @@ goto start
 :force
 cls
 echo DO THIS ONLY IF YOU REALLY NEED TO
-set /p "reason=Enter your reason for this forced commit:"
+set /p reason="Enter your reason for this forced commit:"
 git add .
 git commit -m "%reason%"
-set /p "branch=Which branch do you want to do this to?"
+set /p branch="Which branch do you want to do this to?"
 git push -f origin %branch%
 pause
 goto start
 
 :init
-set /p "origin=What is your origin?"
+set /p origin="What is your origin?"
 git init
 git remote add origin %origin%
 git add .
@@ -124,10 +124,10 @@ pause
 goto start
 
 :initpull
-set /p "origin=What is your origin?"
+set /p origin="What is your origin?"
 git init
 git remote add origin %origin%
-set /p "branch=What is the branch you want to pull from?"
+set /p branch="What is the branch you want to pull from?"
 git pull origin %branch%
 echo finished updating local repository
 pause 
@@ -135,21 +135,21 @@ goto start
 
 :end 
 cls
-set /p "stop=Continue?(y for yes. if yes input anything else)?"
+set /p stop="Continue?(y for yes. if yes input anything else)?"
 if %stop% == y (goto start) 
 exit
 
 :pull
 git branch
-set /p "branch=What branch do you want to pull from?"
+set /p branch="What branch do you want to pull from?"
 git pull origin %branch%
 pause
 goto start
 
 :login
 cls 
-set /p "username=What is your username?"
-set /p "password=What is your password?"
+set /p username="What is your username?"
+set /p password="What is your password?"
 git config --global user.email "%username%"
 git config --global user.password "%password%"
 goto start
