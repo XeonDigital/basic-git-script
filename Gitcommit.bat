87b20f9ec84e03b09976e27e496a1bba8a392e27
XeonDigital <xinrongisaweosm2@gmail.com>
2020-11-22 21:33:52 -0500

    changed to view all local branches

--- Gitcommit.bat
+++ Gitcommit.bat
@@ -1,4 +1,11 @@
 @ECHO OFF
+rem I need to make it so that you would beable to get all of the remote branches and track them
+rem I need to make the script do stashing
+rem I also need to make it so that there are mutiple menus
+rem I also gotta figure out how to error check
+rem I gotta allow it to show git status
+
+
 echo Press enter to continue without opening
 set /p folder="Which folder are you trying to open(Enter folder path or folder name [folder name must be in the current folder])?"
 cd %folder%
@@ -10,21 +17,20 @@ if %input%== y (goto pull)
 :start
 cls
 echo you are in %cd%
+echo Branches:
+echo (The star and the green text is the current branch)
 git branch
 echo -----------------------------------------
 echo 1 - initialize repo
-echo 2 - initialize and pull repo 
+echo 2 - initialize and pull repo
 echo 3 - commit changes
-echo 4 - force commit
+echo 4 - force commit (ONLY DO THIS IF YOU KNOW WHAT YOU ARE DOING)
 echo 5 - set global account
-echo 6 - pull branch
-echo 7 - create branch
-echo 8 - switch branch
-echo 9 - push changes
-echo 10 - merge local branches
-echo 11 - reset commit changes
-echo 12 - change repo
-echo 13 - update current branch from master
+echo 6 - push changes
+echo 7 - reset commit changes
+echo 8 - change directory
+echo 9 - branch menu
+echo 10 - stash menu
 echo -----------------------------------------
 set /p "set=What do you want to do(input the corresponding number)?"
 if defined set (cls) else (goto start)
@@ -33,32 +39,87 @@ if %set%== 2 (goto initpull)
 if %set%== 3 (goto commit)
 if %set%== 4 (goto force)
 if %set%== 5 (goto login)
-if %set%== 6 (goto pull)
-if %set%== 7 (goto createB)
-if %set%== 8 (goto switch)
-if %set%== 9 (goto push)
-if %set%== 10 (goto merge)
-if %set%== 11 (goto reset)
-if %set%== 12 (goto switchRepo)
-if %set%== 13 (goto updateBranch)
+if %set%== 6 (goto push)
+if %set%== 7 (goto reset)
+if %set%== 8 (goto switchRepo)
+if %set%== 9 (goto branchmenu)
+if %set%== 10 (goto stashmenu)
 pause
 goto end
+
+:stashmenu
+echo STASH MENU
+echo Branches:
+echo (The star and the green text is the current branch)
+git branch
+echo Stash List:
+git stash list
+git stash show
+echo -----------------------------------------
+echo 1 - stash current commit
+echo 2 - drop current stash
+echo *Enter nothing to exit
+echo -----------------------------------------
+set /p "set=What do you want to do(input the corresponding number)?"
+if %set%== 1 (goto pull)
+if %set%== 2 (goto createB)
+if %set%== 3 (goto switch)
+if %set%== 4 (goto merge)
+if %set%== 5 (goto fetch)
+pause
+goto start
+
+:branchmenu
+echo BRANCH MENU
+echo Branches:
+echo (The star and the green text is the current branch)
+git branch
+
+echo -----------------------------------------
+echo 1 - pull branch
+echo 2 - create branch
+echo 3 - switch branch
+echo 4 - merge local branches
+echo 5 - fetch all branches
+echo 6 - get all branchs and track them
+echo *Enter nothing to exit
+echo -----------------------------------------
+set /p "set=What do you want to do(input the corresponding number)?"
+if %set%== 1 (goto pull)
+if %set%== 2 (goto createB)
+if %set%== 3 (goto switch)
+if %set%== 4 (goto merge)
+if %set%== 5 (goto fetch)
+pause
+goto start
+
+
+rem methods (idk what to call them)
+
+:fetch
+echo Fetching all the branches
+git fetch
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with fetching
+	pause 
+	goto branchmenu
+)
+goto branchmenu
 
 :switchRepo
 set /p folder="Which folder are you trying to open(Enter folder path)?"
 cd %folder%
 goto start
 
-:updateBranch
-git fetch origin master
-git rebase origin master
-pause
-goto start
-
 
 :reset
 echo Resetting the changes from last commit
 git reset
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with resetting
+	pause 
+	goto start
+)
 echo done
 pause
 goto start
@@ -68,13 +129,23 @@ echo The highlighted green text is where you are right now
 git branch
 set /p merge="Which branch do you want to merge from?"
 git merge %merge%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with merging
+	pause 
+	goto branchmenu
+)
 pause
-goto start
+goto branchmenu
 
 :switch
 git branch
 set /p set="Which branch do you want to switch to?"
 git checkout %set%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with swtiching the branch
+	pause
+	goto start
+)
 pause
 goto start
 
@@ -82,18 +153,43 @@ goto start
 :createB
 set /p branch="What would your branch name be?"
 git checkout -b %branch%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with creating a branch
+	pause
+	goto start
+)
 set /p pull="Which branch do you want to pull from?"
 git pull origin %pull%
 git commit -m "Created a new branch from %pull%"
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with pulling from %pull%
+	pause
+	goto start
+)
 git push origin %branch%
-goto start
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with merging
+	pause
+	goto branchmenu
+)
+goto branchmenu
 
 
 :commit
 cls
 set /p reason="Enter your reason for this commit:"
 git add .
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with adding the files
+	pause
+	goto start
+)
 git commit -m "%reason%"
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with commiting
+	pause
+	goto start
+)
 pause
 goto push
 
@@ -102,12 +198,20 @@ goto push
 git branch
 set /p branch="Which branch do you want to push to?"
 git push origin %branch%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with pushing
+	pause
+	goto start
+)
 pause
 goto start
 
 :force
 cls
 echo DO THIS ONLY IF YOU REALLY NEED TO
+set /p confirm="ARE YOU SURE ABOUT THIS(y=Yes or n=No)?"
+if %confirm%==y (goto start)
+if %confirm%==Y (goto start)
 set /p reason="Enter your reason for this forced commit:"
 git add .
 git commit -m "%reason%"
@@ -119,10 +223,36 @@ goto start
 :init
 set /p origin="What is your origin?"
 git init
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with initializing the repo
+	pause
+	goto start
+)
 git remote add origin %origin%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with adding the remote
+	pause
+	goto start
+)
 git add .
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with adding all the files
+	pause
+	goto start
+)
+set /p comment="Enter the reason or "
 git commit -m "first commit"
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with commiting
+	pause
+	goto start
+)
 git push -u origin master
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with 
+	pause
+	goto start
+)
 echo finished setup
 pause
 goto start
@@ -130,26 +260,40 @@ goto start
 :initpull
 set /p origin="What is your origin?"
 git init
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with initializing the repo
+	pause
+	goto start
+)
 git remote add origin %origin%
 set /p branch="What is the branch you want to pull from?"
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with adding the remote
+	pause
+	goto start
+)
 git pull origin %branch%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with pulling from %branch%
+	pause
+	goto start
+)
 git checkout %branch%
 echo finished updating local repository
 pause 
 goto start
 
-:end 
-cls
-set /p stop="Continue?(y for yes. if yes input anything else)?"
-if %stop% == y (goto start) 
-exit
-
 :pull
 git branch
 set /p branch="What branch do you want to pull from?"
 git pull origin %branch%
+if %ERRORLEVEL% NEQ 0 (
+	echo Something went wrong with pulling from %branch%
+	pause
+	goto branchmenu
+)
 pause
-goto start
+goto branchmenu
 
 :login
 cls 
@@ -158,3 +302,6 @@ set /p password="What is your password?"
 git config --global user.email "%username%"
 git config --global user.password "%password%"
 goto start
+
+:end
+exit
